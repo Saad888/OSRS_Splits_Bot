0ce67b97ebb755a2bb5782a215af91db4da51b70
Xaad <Xaad@users.noreply.github.com>
2019-05-15 16:49:43 -0600

    5/15/2019
    Updated bot testing

---
+++ bot_tests.py
@@ -0,0 +1,33 @@
+# Copied text to test bot
+import discord
+import json
+
+with open('configs.json') as file:
+    configs = json.load(file)
+
+
+TOKEN = configs["Bot Token"]
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+client.run(TOKEN)
